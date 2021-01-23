4a111c35643a2c971bfdf6de5837d85ea133257b
puifaiba <puifai.ba@gmail.com>
2021-01-23 15:30:43 -0500

    commit tree 5d3995a149501b094eb2f6291353f0e5b1837877

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 33, "M", "brown", "affectionate", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 12, "M", "black", "playful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 20, "F", "brown", "shy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 39, "M", "brown", "strict", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 3, "F", "black", "social", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 26, "M", "brown", "mean", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 18, "F", "brown", "cute", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 13, "M", "brown", "violent", 0);
